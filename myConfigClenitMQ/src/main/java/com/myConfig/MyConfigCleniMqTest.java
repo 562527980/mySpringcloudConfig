230132eb86628da8f0a438a55c1eba4283d45dd7
denghaijun <562527980@qq.com>
2019-09-30 14:45:34 +0800

    ii

--- myConfigClenitMQ/src/main/java/com/myConfig/MyConfigCleniMqTest.java
+++ myConfigClenitMQ/src/main/java/com/myConfig/MyConfigCleniMqTest.java
@@ -1,17 +1,23 @@
-/*package com.myConfig;
+package com.myConfig;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
 @RestController
-public class MyConfigClenit {
+@RefreshScope
+public class MyConfigCleniMqTest {
 	
 	public static void main(String[] args) {
-		SpringApplication.run(MyConfigClenit.class, args);
+		SpringApplication.run(MyConfigCleniMqTest.class, args);
 	}
 	
 	@Value("${foo}")
@@ -22,4 +28,3 @@ public class MyConfigClenit {
 	}
 
 }
-*/
